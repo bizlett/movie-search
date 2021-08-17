bf6e0f0ebbbb9a4d686c4009217dc8ad6763f52d
bizlett <bizzyday@gmail.com>
2021-08-17 14:18:38 +0000

    Change OMDb api to TMDB api. Add getWatch function - work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,48 @@
-const API_KEY = "76d72308";
-
 // Global variables
+const API_KEY = 'f0eb98b7c925ef27dc4b795263d8bfe8';
+const url = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
+
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-text');
 const searchButton = document.querySelector('#search-button');
 
 // Event Listeners
 // searchForm.addEventListener('submit', getMovie);
-searchButton.addEventListener('click', getMovie);
+searchButton.addEventListener('click', getWatch);
 
 
 // Functions 
 
 // Search movie function
-function getMovie(e) {
+function getWatch(e) {
     // prevent form from sumitting
     e.preventDefault();
 
     // grab value from form input
     let search = searchInput.value;
 
-    // check if there is text to submit, else trigger alert
+    // check if there is text to submit, then run function
     if (search != "") {
-        alert("You've searched for something!");
+
+        // define parameters 
+        let myUrl = url + '&guery=' + search;
+
+        fetch(myUrl) 
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+        console.log(search);
+
+        // clear search input
+        searchInput.value = '';
 
     } else {
         // Alert message when search box is empty
-        alert("Give us a hint! Search a movie, director or series for more information.");
-    }
-    
+        alert("Give us a hint! Search a person, movie or series for more information.");
+    }    
 };
