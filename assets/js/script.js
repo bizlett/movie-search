f1bcc2be5705d8d3e91c3880fd43669ad09f591e
bizlett <bizzyday@gmail.com>
2021-08-17 12:52:22 +0000

    Add getMovie function structure - test event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@ const API_KEY = "76d72308";
 
 // Global variables
 const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-text');
+const searchButton = document.querySelector('#search-button');
 
 // Event Listeners
-searchForm.addEventListener('submit', getMovie);
+// searchForm.addEventListener('submit', getMovie);
+searchButton.addEventListener('click', getMovie);
+
 
 // Functions 
 
@@ -13,5 +17,16 @@ function getMovie(e) {
     // prevent form from sumitting
     e.preventDefault();
 
+    // grab value from form input
+    let search = searchInput.value;
+
+    // check if there is text to submit, else trigger alert
+    if (search != "") {
+        alert("You've searched for something!");
+
+    } else {
+        // Alert message when search box is empty
+        alert("Give us a hint! Search a movie, director or series for more information.");
+    }
     
 };
