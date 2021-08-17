e21122e86f6bbbbe1d8c3b0731579c94e7069239
bizlett <bizzyday@gmail.com>
2021-08-17 06:49:36 +0000

    Add search button, outline getMovie function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
-const API-KEY = "76d72308"
+const API_KEY = "76d72308";
 
+// Global variables
+const searchForm = document.querySelector('#search-form');
+
+// Event Listeners
+searchForm.addEventListener('submit', getMovie);
+
+// Functions 
+
+// Search movie function
+function getMovie(e) {
+    // prevent form from sumitting
+    e.preventDefault();
+
+    
+};
